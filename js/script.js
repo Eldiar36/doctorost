34ecc94275fef2bcdfb3cd41582676122001836b
unknown <eldiar36>
2020-09-07 18:41:32 +0600

    edit2

--- js/script.js
+++ js/script.js
@@ -67,3 +67,40 @@ $(document).ready(function(){
 
     });
 });
+jQuery(document).ready(function($) {
+    $('.iframe-youtube').magnificPopup({
+        type: 'iframe',
+        height: "100vh",
+
+    });
+});
+
+jQuery(document).ready(function($) {
+    $('.popup-with-form').magnificPopup({
+        type: 'inline',
+        fixedContentPos: true
+
+    });
+});
+
+
+let countDown = new Date('Sep 11, 2020 00:00:00').getTime(),
+    x = setInterval(function() {
+
+        let now = new Date().getTime(),
+            distance = countDown - now;
+
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60 ));
+
+        document.getElementById("days").innerHTML =
+          days ;
+        document.getElementById("hours").innerHTML =
+            hours;
+        document.getElementById("minutes").innerHTML =
+            minutes ;
+    })
+
+
+
